14e231bc1270a294062cc9d1044c4cfda1048ddc
DawidZiec <ziec.dawid@gmail.com>
2019-03-17 16:17:20 +0100

    Commenten bucket.py

--- source/bucket.py
+++ source/bucket.py
@@ -3,24 +3,24 @@ def bucketSort(alist):
     length = len(alist)
     size = largest/length
  
-    buckets = [[] for _ in range(length)]
-    for i in range(length):
+    buckets = [[] for _ in range(length)] #tworzy kubelki rownej wielkosci i wypelnia je czesciami calej listy
+    for i in range(length):               # w ich wlasnych zakresach
         j = int(alist[i]/size)
         if j != length:
             buckets[j].append(alist[i])
         else:
             buckets[length - 1].append(alist[i])
  
-    for i in range(length):
+    for i in range(length): # sortuje poszczugolne kubelki
         insertion_sort(buckets[i])
  
-    result = []
+    result = [] # laczy posortowane kubelki w posortowana liste
     for i in range(length):
         result = result + buckets[i]
  
     return result
  
-def insertion_sort(alist):
+def insertion_sort(alist): # insertion sort dla poszczegolnych kubelkow, nie ma tu rekurencji
     for i in range(1, len(alist)):
         temp = alist[i]
         j = i - 1
